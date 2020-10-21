c993da55d86f2500e0474747d12ef5a8e5250328
FabiolaSaga <saga.fabiola@gmail.com>
2020-10-21 18:16:05 -0500

    Added action to close popup view

---
+++ GOL/GOL/PopupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PopupViewController.swift
+//  GOL
+//
+//  Created by Fabiola S on 10/21/20.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    @IBAction func closePopupPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
